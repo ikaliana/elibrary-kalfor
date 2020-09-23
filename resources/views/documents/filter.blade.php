1b9ee651b953b8f4d33921674b6f3c0eabccf2c7
Indra <ikaliana@yahoo.com>
2020-09-23 14:22:54 +0700

    Update filter.blade.php
    generate category list automatically

--- resources/views/documents/filter.blade.php
+++ resources/views/documents/filter.blade.php
@@ -1,6 +1,15 @@
 	<div class="text-right" style="margin-top:30px;margin-bottom: -70px;">
 		<span class="mr-3 font-weight-bold">Categories: </span> 
+		@foreach($category as $cat)
 		<div class="form-check form-check-inline">
+			<label class="form-check-label">
+				<input id="category-check-{{ $cat['id'] }}" class="form-check-input" type="checkbox" checked="" value="category-{{ $cat['id'] }}">
+				<span class="form-check-sign"></span> {{ $cat['name'] }}
+			</label>
+		</div>
+		@endforeach
+		
+		<!-- <div class="form-check form-check-inline">
 			<label class="form-check-label">
 				<input id="category-check-1" class="form-check-input" type="checkbox" checked="" value="category-1">
 				<span class="form-check-sign"></span> N/A
@@ -29,6 +38,6 @@
 				<input id="category-check-5" class="form-check-input" type="checkbox" checked="" value="category-5">
 				<span class="form-check-sign"></span> Module
 			</label>
-		</div>
+		</div> -->
     </div>
 
