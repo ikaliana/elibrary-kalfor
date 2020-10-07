55cac88ba817ced995be34251244425c67b43ee1
Indra <ikaliana@yahoo.com>
2020-10-07 15:38:01 +0700

    Create pagingcss.blade.php
    link to css and custom css for pagination

---
+++ resources/views/documents/pagingcss.blade.php
@@ -0,0 +1,19 @@
+<link href="{{ asset('css/jquery.smFilteredPagination.css') }}" rel="stylesheet">
+<style type="text/css">
+	.pagination a, .pagination span {
+		font-size: 14px !important;
+		min-width: 30px !important;
+		border: none;
+		border-radius: 30px;
+		margin-left: 4px;
+	}
+
+	.pagination .current {
+		background-color: #f96332;
+		color: #fff;
+	}
+
+	.pagination {
+		margin-bottom: 30px;
+	}
+</style>
