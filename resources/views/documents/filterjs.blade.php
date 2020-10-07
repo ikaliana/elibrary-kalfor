c9cc7ee5a9b2fe46ef7fb2ae3321b3d2c33ead84
Indra <ikaliana@yahoo.com>
2020-10-07 15:37:18 +0700

    Update filterjs.blade.php
    update filter to comply with paging library

--- resources/views/documents/filterjs.blade.php
+++ resources/views/documents/filterjs.blade.php
@@ -1,8 +1,14 @@
 <script type="text/javascript">
 	$(".form-check-input").on("change", function(e,a) {
 		var class_name = $(this).val();
-		var class_attr = ($(this).is(':checked')) ? "flex" : "none";
+		//var class_attr = ($(this).is(':checked')) ? "flex" : "none";
+		//$("li." + class_name).css("display",class_attr);
 
-		$("li." + class_name).css("display",class_attr);
+		if($(this).is(':checked')) $("li." + class_name).removeClass("filtered");
+		else $("li." + class_name).addClass("filtered");
+
+		pagination2.rebuild();
+		SetPage(1);
+		//$(".pagination").find("a").on("click",PageClick);
 	});
 </script>
