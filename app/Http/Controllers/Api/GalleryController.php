b52dd92b3817d62c0837daeaf375b009807d65e2
Indra <ikaliana@yahoo.com>
2021-01-19 22:37:32 +0700

    Update GalleryController.php
    Get user ID directly from session

--- app/Http/Controllers/Api/GalleryController.php
+++ app/Http/Controllers/Api/GalleryController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Gallery;
+use GuzzleHttp\Client;
+use GuzzleHttp\Exception\RequestException;
 
 class GalleryController extends Controller
 {
@@ -130,7 +132,9 @@ class GalleryController extends Controller
 
     private function get_current_user_id(Request $request)
     {
-        return 1;
+        $user_info = $this->Userinfo($request);
+
+        return $user_info["id"];
     }
 
     private function validate_owner(Request $request, $gallery)
@@ -164,4 +168,43 @@ class GalleryController extends Controller
 
         return $gallery;
     }
+
+    protected function InitClient() {
+        return new Client([
+            'verify' => false,
+            'curl' => [
+                CURLOPT_SSL_VERIFYHOST => false,
+                CURLOPT_SSL_VERIFYPEER => false
+            ]
+        ]);
+    }
+
+    protected function OpenRequest($method, $url, $params) {
+        $client = $this->InitClient();
+
+        try {
+            $response = $client->request($method, $url, $params);
+            return $response;
+        } catch (RequestException $e) {
+            if($e->getCode() == 401) return $e->getResponse();
+            else abort($e->getCode(), $e->getResponse()->getBody());
+        }
+    }
+
+    protected function Userinfo(Request $request) {
+        $response = $this->OpenRequest('GET',env('SSO_SERVER_URL').'api/user', 
+            ['headers' => [
+                'Authorization' => 'Bearer '.$request->bearerToken(),
+                'Accept' => 'application/json',
+                ]
+            ]);
+
+        $statusCode = $response->getStatusCode();
+
+        if($statusCode == '200') 
+            return json_decode((string) $response->getBody(), true);
+        else 
+            abort($statusCode, $response->getBody());
+    }
+
 }
